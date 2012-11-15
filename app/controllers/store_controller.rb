5b66691d2fcf6d3a02162f19ad723fc39dc8c1db
Janikole <jacquiehiebert@hotmail.com>
2012-11-15 13:12:02 -0600

    added a purchase page and a shopping cart page. Neither one does anything

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,9 +1,20 @@
 class StoreController < ApplicationController
+  
   def index
     @products = Product.all()
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+  
+  def purchase
+    @product = Product.find(params[:id])
+    @customer = Customer.new
+    @customer.orders.build
+    #@order_product = @order.order_products.build
+  end
+  
+  def create
   end
 end
