b1ed79eaabd6da5aa5ab105020e4500be497d7f2
Janikole <jacquiehiebert@hotmail.com>
2012-11-15 01:10:12 -0600

    Added images column to products

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ActiveRecord::Base
   belongs_to :province
   has_many :orders
+  
+  validates_presence_of :province, :f_name, :l_name, :city, :address, :postal_code
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   has_many :order_products
   has_many :products, :through => :order_products
+  
+  validates_presence_of :cust_id, :gst_rate, :pst_rate, :hst_rate, :status, :payment_method
+  validates_inclusion_of :status, :in => ['Paid', 'Shipped']
 end
