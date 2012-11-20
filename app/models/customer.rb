47b47d14cecb7626ed8bce40ac0ebb9a310496f2
Janikole <jacquiehiebert@hotmail.com>
2012-11-20 11:37:02 -0600

    Customer deleteions will result in all associated orders being deleted

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ActiveRecord::Base
   belongs_to :province
-  has_many :orders
+  has_many :orders, :dependent => :delete_all
   
   validates_presence_of :province, :f_name, :l_name, :city, :address, :postal_code
 end
